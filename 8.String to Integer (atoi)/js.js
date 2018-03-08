b06c3d3003de61388146eee7dc0e029b77bb0b26
Timothy <tim77813@gmail.com>
2018-03-09 00:24:49 +0800

    8. String to Integer (atoi) - update correct verison

--- 8.String to Integer (atoi)/js.js
+++ 8.String to Integer (atoi)/js.js
@@ -2,23 +2,56 @@
  * @param {string} str
  * @return {number}
  */
-var myAtoi = function (str) {
+var myAtoi = function(str) {
+    //no need to parse empty string
+    if (str.length == 0) return 0;
+
     const INT_MAX = 2147483647,
-        INT_MIN = -2147483648;
+        INT_MIN = -2147483648,
+        NUMBER = "0123456789";
 
-    var signDetected = false,
-        isStringThroughing = false,
-        isSignExist = false;
-    for (let i = 0; i < str.length; ++i) {
-        console.log(str[i]);
+    //remove empty part
+    var index = 0;
+    while (str[index] == ' ') {
+        ++index;
+    }
+    str = str.slice(index, str.length);
+    if (str.length == 0) return 0;
 
-        var char = str[i];
-        if(char == '' && !isStringThroughing)
-            continue;
-        
+    //extract number part
+    var sign = null,
+        number = null;
+
+    if (str[0] == "+" || str[0] == "-") {
+        sign = str.slice(0, 1);
+        str = str.slice(1, str.length);
+        if (str.length == 0) return 0;
     }
 
+    index = 0;
+    while (NUMBER.indexOf(str[index]) != -1) ++index;
+    number = str.slice(0, index);
 
+    //convert to number
+    number = arrToNumber(number);
+
+    if (sign != null && sign == "-") {
+        number = -1 * number;
+    }
+
+    if (number > INT_MAX) return INT_MAX;
+    else if (number < INT_MIN) return INT_MIN;
+    else return number;
+
+    function arrToNumber(arr) {
+        var length = arr.length,
+            result = 0;
+
+        for (let i = 0; i < length; ++i) {
+            result += arr[i] * Math.pow(10, (length - i - 1));
+        }
+        return result;
+    }
 };
 
 var string = "    +51651adawdgg";
